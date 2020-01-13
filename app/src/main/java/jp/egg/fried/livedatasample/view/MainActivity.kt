dcd78aa12b5f92474ff75cdd0cd929b594deeb1d
Yoshiyasu <te108128@gmail.com>
2020-01-14 00:42:07 +0900

    Apply DataBinding: MainActivity

--- app/src/main/java/jp/egg/fried/livedatasample/view/MainActivity.kt
+++ app/src/main/java/jp/egg/fried/livedatasample/view/MainActivity.kt
@@ -2,7 +2,9 @@ package jp.egg.fried.livedatasample.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import jp.egg.fried.livedatasample.R
+import jp.egg.fried.livedatasample.databinding.ActivityMainBinding
 import jp.egg.fried.livedatasample.module.viewModelModule
 import jp.egg.fried.livedatasample.viewmodel.MainViewModel
 import org.koin.android.ext.android.startKoin
@@ -11,14 +13,23 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class MainActivity : AppCompatActivity() {
 
     //region: properties
-    private val binding
+    private var binding: ActivityMainBinding? = null
     private val viewModel: MainViewModel by viewModel()
     //endregion
 
+
+    //region: override MainActivity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(
+            this,
+            R.layout.activity_main
+        ).apply {
+            lifecycleOwner = this@MainActivity
+        }
     }
+    //endregion
 
 
 }
