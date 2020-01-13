cab4aeec7017c8af7ac53d059d5565b642054576
Yoshiyasu <te108128@gmail.com>
2020-01-14 01:32:56 +0900

    add buttonText LiveData

--- app/src/main/java/jp/egg/fried/livedatasample/viewmodel/MainViewModel.kt
+++ app/src/main/java/jp/egg/fried/livedatasample/viewmodel/MainViewModel.kt
@@ -18,4 +18,28 @@ class MainViewModel : ViewModel() {
     val buttonText: LiveData<String> = _buttonText
     //endregion
 
+    //region: properties
+    private val buttonTextList: List<String> = listOf(
+        "テキストが変わるボタン",
+        "何かに変わるよ",
+        "同じテキストが選ばれることもあるよ",
+        "どれに変わるかな？",
+        "これはLiveDataのサンプルだよ",
+        "ここを押すとLiveDataが更新されるよ"
+    )
+    //endregion
+
+
+    //region: public methods
+    fun initialize() {
+        // 初期値はリストの先頭
+        _buttonText.value = buttonTextList.first()
+    }
+
+    fun onButtonClick() {
+        // リストからランダムでテキストを選ぶ
+        _buttonText.value = buttonTextList[(0..buttonTextList.lastIndex).shuffled().first()]
+    }
+    //endregion
+
 }
